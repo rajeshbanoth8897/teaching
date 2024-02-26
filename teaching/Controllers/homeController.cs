88ac23cc0648a78b82b2244a35a3ca578f76c697
labuser <labuser@ORACLE>
2024-02-26 07:25:47 +0000

    Add project files.

---
+++ teaching/Controllers/homeController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using teaching.Models;
+
+namespace teaching.Controllers
+{
+    public class homeController : Controller
+    {
+        teachingEntities1 dc = new teachingEntities1();
+        // GET: home
+        public ActionResult Home()
+        {
+            return View();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+       
+        [HttpPost]
+        public ActionResult teacherlogin(string Username, string Password)
+        {
+
+            if (Username == "rajesh" && Password == "india")
+            {
+                return RedirectToAction("Index");
+            }
+            else 
+                return View();
+        }
+        public ActionResult teacherlogin()
+        {
+
+            return View();
+        }
+        public ActionResult studentregi()
+        {
+            return View();
+        }
+
+        public ActionResult studentlgn()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult studentlgn(studentlgn ob)
+        {
+            //dc.studentlgns.Add(ob);
+            //int i = dc.SaveChanges();
+            //if (i > 0)
+            //    ViewData["e"] = "record inserted";
+            //else
+            //    ViewData["e"] = "insertion failed";
+
+
+            var res = (from t in dc.studentlgns
+                       where t.studentid == ob.studentid && t.pwd == ob.pwd
+                       select t).Count();
+            if (res > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewData["e"] = "invalid credentials";
+            }
+            return View();
+        }
+        [HttpPost]
+        public ActionResult studentregi(studentlgn ob)
+        {
+            dc.studentlgns.Add(ob);
+            int i = dc.SaveChanges();
+            if (i > 0)
+                ViewData["e"] = "record inserted";
+            else
+                ViewData["e"] = "insertion failed";
+            return View();
+        }
+
+        public ActionResult pdfrecords()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ teaching/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("teaching")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("teaching")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d5ec2394-b952-4438-87f1-476b27e602e7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
